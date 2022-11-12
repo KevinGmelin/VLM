83e4cda173c3d7f09ac532d644bdbc0103a175b4
jsongCMU <jaekyuns@andrew.cmu.edu>
2022-11-11 21:45:52 -0500

    Added clipwrap import

--- models/SemanticStream.py
+++ models/SemanticStream.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from CLIPWrapper import CLIPWrapper
 from blocks.ConvBlock import ConvBlock
 from blocks.UpBlock import UpBlock
 
